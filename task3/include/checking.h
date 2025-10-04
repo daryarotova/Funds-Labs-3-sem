d4b99cc6cb0b256de6e80351b06cf30ccf01cd70
daryarotova <149077008+daryarotova@users.noreply.github.com>
2025-10-04 14:47:42 +0300

    Delete task3 directory

--- task3/include/checking.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHECKING_H
-#define CHECKING_H
-
-#include "errors.h"
-
-errorCodes isRightDoubleNum(const char *str); // проверяет, является ли строка корректным числом с плавающей точкой
-errorCodes isRightIntNum(const char *str); // проверяет, является ли строка корректным целым числом
-
-#endif // CHECKING_H
--- task3/include/solving.h
+++
@@ -1,9 +0,0 @@
-#ifndef SOLVING_H
-#define SOLVING_H
-
-#include "errors.h"
-
-errorCodes solveQuadraticEquation(double epsilon, double a, double b, double c, double *roots); // квадратное уравнение ax^2 + bx + c = 0, результат записывается в roots (должен быть массив размером 2)  
-errorCodes isRightTriangle(double epsilon, double sideA, double sideB, double sideC, int *result);
-
-#endif // SOLVING_H
