fc443886e1810dfc3c502558bf70175bc1d8b77b
daryarotova <149077008+daryarotova@users.noreply.github.com>
2025-10-07 23:13:05 +0300

    Create constants.h

---
+++ lab1/task7/include/constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+typedef enum {
+    OK = 0,
+    INVALID_ARGUMENT = 1,
+    PARSE_ERROR = 2,
+    NO_MEMORY = 3,
+    OVERFLOW = 4,
+    IO_ERROR = 5,
+} errorCodes;
+
+#endif
